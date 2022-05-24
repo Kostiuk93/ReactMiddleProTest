d53953f09935b3cc8816a5295ca53088e5e1421e
Kostiuk93 <kostiuk93@gmail.com>
2022-05-25 02:24:18 +0300

    ReactMiddleProTest useCallback()

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
-import {Component, useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
-
-// class Slider extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
 
 const calcValue = () => {
     console.log('random')
@@ -58,24 +13,23 @@ const Slider = (props) => {
     const [slide, setSlide]= useState(0);
     const [autoplay, setAutiplay]= useState(false);
 
+    const getSomeImg = useCallback(() => {
+        console.log('fetchig')
+        return [
+            "https://avatars.mds.yandex.net/i?id=8cb608a4347ac3303f920ca182aa3b90-5887129-images-thumbs&n=13&exp=1",
+            "https://avatars.mds.yandex.net/i?id=f336fbfa4b5852c8d731420d08da64e4-4114158-images-thumbs&n=13&exp=1"
+        ]
+    }, [slide])
+
     function logging () {
         console.log('log')
     }
 
     useEffect(() => {
-        console.log('effect')
         document.title = `Slide: ${slide}`
-
-        window.addEventListener('click', logging)
-
-        return () => {
-            window.removeEventListener('click', logging)
-        }
-
     }, [slide])
 
     useEffect(() => {
-        console.log('autoplay')
     }, [autoplay])
 
     function changeSlide(i) {
@@ -89,6 +43,17 @@ const Slider = (props) => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+
+                {/* {
+                    getSomeImg().map((url, i) => {
+                        return (
+                            <img key={i} className="d-block w-100" src={url} alt="slide" />
+                        )
+                    })
+                } */}
+
+                <Slide getSomeImg={getSomeImg}/>
+
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                 {autoplay ? 'auto' : null}
                 </div>
@@ -115,11 +80,22 @@ const Slider = (props) => {
     )
 }
 
+const Slide = ({getSomeImg}) => {
+    const [images, setImages] = useState([])
+    useEffect(() => {
+        setImages(getSomeImg())
+    }, [getSomeImg])
+
+    return (
+        <>
+            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
+        </>
+    )
+}
+
 
 function App() {
     const [slider, setSlider] = useState(true)
-    
-
   return (
         <>
             <button onClick={() => setSlider(false)}>Click</button>
