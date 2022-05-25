a92e02d1013ed36e889a6cdffe1feb5778a30408
Kostiuk93 <kostiuk93@gmail.com>
2022-05-25 23:02:54 +0300

    ReactMiddleProTest useRef()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import {useState, useEffect, useCallback, useMemo} from 'react';
+import {useState, useEffect, useCallback, useMemo, useRef} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+
+const Form = () => {
+    const myRef = useRef(null)
+
+    const focusFirstTI = () => {
+        myRef.current.focus();
+    }
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input ref={myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea onClick={focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
 
 const calcValue = () => {
     console.log('random')
@@ -113,6 +136,7 @@ function App() {
         <>
             <button onClick={() => setSlider(false)}>Click</button>
             {slider ? <Slider/> : null}
+            <Form/>
         </>
   );
 }
