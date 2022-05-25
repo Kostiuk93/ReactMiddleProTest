12418041aa0af4f068553c4db1bbdf4718d1b094
Kostiuk93 <kostiuk93@gmail.com>
2022-05-25 03:01:36 +0300

    ReactMiddleProTest useMemo()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
 const calcValue = () => {
     console.log('random')
-
     return Math.random() * (50 - 1) +1
+}
+
+const countTotal = (num) => {
+    console.log('counting...')
+    return num + 10
 }
 
 const Slider = (props) => {
@@ -13,6 +17,7 @@ const Slider = (props) => {
     const [slide, setSlide]= useState(0);
     const [autoplay, setAutiplay]= useState(false);
 
+    // при useCallback() кэшируется ФУНКЦИЯ
     const getSomeImg = useCallback(() => {
         console.log('fetchig')
         return [
@@ -39,24 +44,32 @@ const Slider = (props) => {
     function toggleAutoplay() {
         setAutiplay(autoplay => !autoplay)
     }
+
+    // при useMemo() кэшируется ЗНАЧЕНИЕ
+    const total = useMemo(() => {
+        return countTotal(slide)
+    }, [slide])
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide])
+    
+    useEffect(() => {
+        console.log('styles!')
+    }, [style])
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-
-                {/* {
-                    getSomeImg().map((url, i) => {
-                        return (
-                            <img key={i} className="d-block w-100" src={url} alt="slide" />
-                        )
-                    })
-                } */}
 
                 <Slide getSomeImg={getSomeImg}/>
 
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                 {autoplay ? 'auto' : null}
                 </div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
+
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
