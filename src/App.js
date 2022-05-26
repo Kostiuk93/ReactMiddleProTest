d8a9d78c8f75abb49c904c32dab3d478b186a382
Kostiuk93 <kostiuk93@gmail.com>
2022-05-26 23:32:37 +0300

    React Middle Pro Test Собственный хук

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import {useState, useEffect, useCallback, useMemo, useRef} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-const Form = () => {
-    const myRef = useRef(null)
+// СОБСТВЕННЫЙ ХУК / CASTOM HOOK
+function useInputWithValidate (initialValue) {
+    const [value, setValue] = useState(initialValue)
 
-    const focusFirstTI = () => {
-        myRef.current.focus();
+    const onChange = event => {
+        setValue(event.target.value)
     }
+
+    const valideteInput = () => {
+        // return text.search(/\d/) >= 0 ? true : false
+        return value.search(/\d/) >= 0 
+    }
+
+    return {value, onChange, valideteInput}
+}
+
+
+const Form = () => {
+    
+    const input = useInputWithValidate('')
+    const textArea = useInputWithValidate('')
+
+    
+
+    const color = input.valideteInput() ? 'text-danger' : null
 
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                    <input ref={myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    <input value={`${input.value} / ${textArea.value}`} type="text" className='form-control' />
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3" >Email address</label>
+                    <input 
+                    onChange={input.onChange} 
+                    value={input.value}
+                    type="email" 
+                    className={`form-control ${color}` }
+                    id="exampleFormControlInput1" 
+                    placeholder="name@example.com"/>
                     </div>
                     <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea onClick={focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea 
+                    onChange={textArea.onChange}
+                    value={textArea.value}
+                    className="form-control" 
+                    id="exampleFormControlTextarea1" 
+                    rows="3"></textarea>
                 </div>
             </form>
         </Container>
@@ -134,8 +165,8 @@ function App() {
     const [slider, setSlider] = useState(true)
   return (
         <>
-            <button onClick={() => setSlider(false)}>Click</button>
-            {slider ? <Slider/> : null}
+            {/* <button onClick={() => setSlider(false)}>Click</button> */}
+            {/* {slider ? <Slider/> : null} */}
             <Form/>
         </>
   );
